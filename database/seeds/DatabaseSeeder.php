b845c16f632c8e418271a3370a1d8b22bc92aee4
Alan Ibarra <alanibarralop@gmail.com>
2017-12-10 13:28:22 -0700

    Feat: modificando seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('configuraciones')->insert([
             'usuario' => 'admin',
-            'pwd' => md5('admin')
+            'pwd' => md5('admin'),
+            'horario' => '{"duracion":"+15 minute","lunes":{"inicio":"9:00:00","fin":"18:00:00","inini":"14:00:00","infin":"16:00:00"},"martes":{"inicio":"9:00:00","fin":"18:00:00","inini":"14:00:00","infin":"16:00:00"},"miercoles":{"inicio":"9:00:00","fin":"18:00:00","inini":"14:00:00","infin":"16:00:00"},"jueves":{"inicio":"9:00:00","fin":"18:00:00","inini":"14:00:00","infin":"16:00:00"},"viernes":{"inicio":"9:00:00","fin":"18:00:00","inini":"14:00:00","infin":"16:00:00"},"sabado":{"inicio":"9:00:00","fin":"14:00:00","inini":null,"infin":null},"domingo":{"inicio":null,"fin":null,"inini":null,"infin":null}}'
         ]);
 
         DB::table('pacientes')->insert([
@@ -121,7 +122,8 @@ class DatabaseSeeder extends Seeder
             ['grupo' => strtoupper('Carnes'),'proteinas' => 14, 'grasas' => 12,'energia' => 15,'carbohidratos' => 30],
             ['grupo' => strtoupper('Legumbres'),'proteinas' => 15, 'grasas' => 11,'energia' => 12,'carbohidratos' => 15],
             ['grupo' => strtoupper('Cereales'),'proteinas' => 16, 'grasas' => 10,'energia' => 25,'carbohidratos' => 19],
-            ['grupo' => strtoupper('Lacteos'),'proteinas' => 17, 'grasas' => 9,'energia' => 23,'carbohidratos' => 12]
+            ['grupo' => strtoupper('Lacteos'),'proteinas' => 17, 'grasas' => 9,'energia' => 23,'carbohidratos' => 12],
+            ['grupo' => strtoupper('Frutas'),'proteinas' => 10, 'grasas' => 4,'energia' => 10,'carbohidratos' => 11]
 
         ]);
 
@@ -130,22 +132,53 @@ class DatabaseSeeder extends Seeder
             ['descripcion'=>strtoupper('Sopa de verduras'),'um'=>'medio litro','grupo_id' => 1],
             ['descripcion'=>strtoupper('Carne asada'),'um'=>'dos filetes','grupo_id' => 2],
             ['descripcion'=>strtoupper('Cereal'),'um'=>'una taza de leche','grupo_id' => 4],
-            ['descripcion'=>strtoupper('Yogurth'),'um'=>'una taza' ,'grupo_id' => 5]
+            ['descripcion'=>strtoupper('Yogurth'),'um'=>'una taza' ,'grupo_id' => 5],
+            ['descripcion'=>strtoupper('Sandwich de pollo'),'um'=>'1 sandwich' ,'grupo_id' => 5],
+
+            ['descripcion'=>strtoupper('Huevos'),'um'=>'dos huevos al gusto' ,'grupo_id' => 5],
+            ['descripcion'=>strtoupper('Salmon'),'um'=>'1 filete' ,'grupo_id' => 2],
+            ['descripcion'=>strtoupper('Patata hervida'),'um'=>'1 patata' ,'grupo_id' => 3],
+            ['descripcion'=>strtoupper('Atun'),'um'=>'1 sobre' ,'grupo_id' => 2],
+            ['descripcion'=>strtoupper('Zanahoria'),'um'=>'dos zanahorias' ,'grupo_id' => 1],
+            ['descripcion'=>strtoupper('Fresas'),'um'=>'tres fresas' ,'grupo_id' => 6],
+
+            ['descripcion'=>strtoupper('Cafe'),'um'=>'1 taza' ,'grupo_id' => 5],
+            ['descripcion'=>strtoupper('Jugo de naranja'),'um'=>'1 vaso' ,'grupo_id' => 6],
+            ['descripcion'=>strtoupper('Agua de limon'),'um'=>'2 vaso' ,'grupo_id' => 6],
+
         ]);
 
         DB::table('menus')->insert([
-            ['nombre' => strtoupper('MenuSeeder'),'energia' => 43,'grasas' => 37,'proteinas' => 40,'carbohidratos' => 83]
+            ['nombre' => strtoupper('nutrete Desayuno inicial'),'energia' => 20,'grasas' => 5,'proteinas' => 23,'carbohidratos' => 10],
+            ['nombre' => strtoupper('nutrete Comida inicial'),'energia' => 15,'grasas' => 6,'proteinas' => 15,'carbohidratos' => 14],
+            ['nombre' => strtoupper('nutrete Cena inicial'),'energia' => 19,'grasas' => 5,'proteinas' => 20,'carbohidratos' => 12],
+            ['nombre' => strtoupper('nutrete colacion inicial Matutina'),'energia' => 20,'grasas' => 2,'proteinas' => 15,'carbohidratos' => 15],
+            ['nombre' => strtoupper('nutrete colacion inicial Vespertina'),'energia' => 19,'grasas' => 4,'proteinas' => 10,'carbohidratos' => 15]
         ]);
 
         DB::table('det_ali_men')->insert([
-            ['alimento_id' => 1 , 'menu_id' => 1 , 'porciones' => 2],
-            ['alimento_id' => 2 , 'menu_id' => 1 , 'porciones' => 1],
-            ['alimento_id' => 3 , 'menu_id' => 1 , 'porciones' => 2],
+            ['alimento_id' => 7 , 'menu_id' => 1 , 'porciones' => 2],
+            ['alimento_id' => 6 , 'menu_id' => 1 , 'porciones' => 1],
+            ['alimento_id' => 14 , 'menu_id' => 1 , 'porciones' => 1],
+
+            ['alimento_id' => 1 , 'menu_id' => 2, 'porciones' => 2],
+            ['alimento_id' => 2 , 'menu_id' => 2, 'porciones' => 1],
+            ['alimento_id' => 15 , 'menu_id' => 2, 'porciones' => 1],
+
+            ['alimento_id' => 4 , 'menu_id' => 3, 'porciones' => 1],
+            ['alimento_id' => 9, 'menu_id' => 3, 'porciones' => 1],
+            ['alimento_id' => 13, 'menu_id' => 3, 'porciones' => 1],
+
+            ['alimento_id' => 11, 'menu_id' => 4, 'porciones' => 2],
+            ['alimento_id' => 12, 'menu_id' => 4, 'porciones' => 3],
+
+            ['alimento_id' => 5, 'menu_id' => 5, 'porciones' => 1],
+
         ]);
 
-        DB::table('det_pac_men')->insert([
+        /*DB::table('det_pac_men')->insert([
             ['paciente_id' => 1 , 'menu_id' => 1]
-        ]);
+        ]);*/
 
     }//
 
